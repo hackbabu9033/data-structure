589d2d13a38287f177deb3de0b883c6ed4077360
hank <hank@wishingsoft.com>
2021-01-29 22:13:42 +0800

    add circle queue

---
+++ ch3-queue-and-stack/ch3-queue-and-stack/CircleQueue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ch3_queue_and_stack
+{
+    public class CircleQueue<T>
+    {
+        private int Length { set; get; }
+        private int Front { set; get; }
+        private int Rear { set; get; }
+
+        private T[] Items { set; get; }
+        public CircleQueue(int length)
+        {
+            Length = length;
+            Front = -1;
+            Rear = -1;
+            Items = new T[Length];
+        }
+
+        public void Enqueue(T item)
+        {
+            var nextRear = (Rear + 1) % Length;
+
+            if (Front == -1)
+            {
+                Front = 0;
+                Items[nextRear] = item;
+                Rear = nextRear;
+                return;
+            }
+         
+            if (Front == nextRear)
+            {
+                Console.WriteLine("queue is full");
+                return;
+            }
+            Items[nextRear] = item;
+            Rear = nextRear;
+            Console.WriteLine("item is enqueued successfully");
+        }
+
+        public void Dequeue()
+        {
+            var nextFront = (Front + 1) % Length;
+            if (nextFront > Rear)
+            {
+                Console.WriteLine("queue is empty, nothing to delete !!");
+                return;
+            }
+            Items[Front] = default;
+            Front = nextFront;
+        }
+    }
+}
---
+++ ch3-queue-and-stack/ch3-queue-and-stack/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ch3_queue_and_stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intCircleQueue = new CircleQueue<int>(5);
+            intCircleQueue.Enqueue(1);
+            intCircleQueue.Enqueue(2);
+            intCircleQueue.Enqueue(3);
+            intCircleQueue.Enqueue(4);
+            intCircleQueue.Enqueue(5);
+            intCircleQueue.Dequeue();
+            intCircleQueue.Dequeue();
+            intCircleQueue.Dequeue();
+        }
+    }
+}
