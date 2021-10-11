d2166fe4b39498630a54775a931bb3c341dbd6ca
hackbabu <hack870629@gmail.com>
2021-10-11 14:22:11 +0800

    加上binary search

---
+++ SortingAndSearch/SortingAndSearch/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SortingAndSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> test = new List<int>() { 1, 14, 63, 88, 100, 145 };
+            List<int> result = new List<int>();
+            result.Add(Search.BinarySearchIndex(test,1));
+            result.Add(Search.BinarySearchIndex(test,14));
+            result.Add(Search.BinarySearchIndex(test,63));
+            result.Add(Search.BinarySearchIndex(test,88));
+            result.Add(Search.BinarySearchIndex(test,100));
+            result.Add(Search.BinarySearchIndex(test,145));
+            result.Add(Search.BinarySearchIndex(test,1111));
+            
+        }
+    }
+}
---
+++ SortingAndSearch/SortingAndSearch/Search.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SortingAndSearch
+{
+    public static class Search
+    {
+        public static int BinarySearchIndex(List<int> sortedList,int search)
+        {
+            int left,right,middle,value;
+            left = 0;
+            middle = 0;
+            right = sortedList.Count - 1;
+            while (right >= left)
+            {
+                middle = (left + right) / 2;
+                value = sortedList[middle];
+                if (search == value)
+                {
+                    return middle;
+                }
+                else if (search > value)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+            return sortedList[middle] == search ? middle : -1;
+        }
+    }
+}
